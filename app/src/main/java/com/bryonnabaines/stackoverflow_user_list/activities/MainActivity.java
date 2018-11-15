ac1eeda85fa1d37650f0138f6f9e4658a3d2651b
Bryonna Baines <bbaines09@gmail.com>
2018-11-15 10:47:42 -0800

    updated main activity and refactored file names so they're more conventional

--- app/src/main/java/com/bryonnabaines/stackoverflow_user_list/activities/MainActivity.java
+++ app/src/main/java/com/bryonnabaines/stackoverflow_user_list/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bryonnabaines.stackoverflow_user_list.activities;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,16 +8,48 @@ import com.bryonnabaines.stackoverflow_user_list.R;
 
 import com.bryonnabaines.stackoverflow_user_list.fragments.UserListFragment;
 
-public class MainActivity extends AppCompatActivity {
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+
+public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
+
+        this.configureDagger();
+        this.showFragment(savedInstanceState);
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return dispatchingAndroidInjector;
+    }
+
+    private void showFragment(Bundle savedInstanceState){
         if (savedInstanceState == null) {
+            // TODO remove this stuff if we don't need to send anything to fragment/viewModel
+//            UserListFragment fragment = new UserListFragment();
+//            Bundle bundle = new Bundle();
+
+//            bundle.putString(SOMETHING_TO_PASS);
+//            fragment.setArguments(bundle);
+
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, UserListFragment.newInstance())
+                    .add(R.id.fragment_container, UserListFragment.newInstance())
                     .commitNow();
         }
+    }
+
+    private void configureDagger(){
+        AndroidInjection.inject(this);
     }
 }
